214e35ca93634930eafc3b2f6684f3e67ccb75cc
TheMichaelBradley44 <michaelb44@live.co.uk>
2024-11-05 01:52:51 +0000

    Setting up Wave system and collectibles

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-
     public float speed;
     private Rigidbody enemyRb;
     private GameObject player;
@@ -22,5 +21,10 @@ public class Enemy : MonoBehaviour
         Vector3 lookDirection = (player.transform.position - transform.position).normalized;
         
         enemyRb.AddForce(lookDirection * speed);
+
+        if (transform.position.y < -10)
+        {
+            Destroy(gameObject);
+        }
     }
 }
