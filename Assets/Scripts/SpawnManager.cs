afa58be4c6d9d759f05e488f483f59ce8412dcfb
TheMichaelBradley44 <michaelb44@live.co.uk>
2024-11-05 01:27:23 +0000

    Powerup and indicators

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -6,24 +6,27 @@ public class SpawnManager : MonoBehaviour
 {
 
     public GameObject enemyPrefab;
-    private float spawnRange = 9;
+    private float spawnRange = 9.0f;
     
     // Start is called before the first frame update
     void Start()
     {
         Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
     }
-    
-    private Vector3 GenerateSpwanPosition()
-    {
-        float spawnPosX = Random.Range(-spawnRange, spawnRange);
-        float spawnPosZ = Random.Range(-spawnRange, spawnRange);
-        Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
-    }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    
+    private Vector3 GenerateSpawnPosition()
+    {
+        float spawnPosX = Random.Range(-spawnRange, spawnRange);
+        float spawnPosZ = Random.Range(-spawnRange, spawnRange);
+        
+        Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
+
+        return randomPos;
     }
 }
